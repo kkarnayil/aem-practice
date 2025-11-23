bbea34864823f5b5c29ddbb8b6154b8b606e2b85
Kartik <kkarnayil@gmail.com>
2025-11-23 10:38:47 +0530

    Add Support for internal links

--- core/src/main/java/com/aempactice/core/models/ExtendedListImpl.java
+++ core/src/main/java/com/aempactice/core/models/ExtendedListImpl.java
@@ -1,23 +1,30 @@
 package com.aempactice.core.models;
 
 import com.adobe.cq.export.json.ExporterConstants;
+import com.adobe.cq.wcm.core.components.commons.link.Link;
+import com.adobe.cq.wcm.core.components.commons.link.LinkManager;
 import com.adobe.cq.wcm.core.components.models.List;
 import com.adobe.cq.wcm.core.components.models.ListItem;
+import com.day.cq.wcm.api.Page;
 import lombok.Getter;
 import lombok.experimental.Delegate;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Exporter;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
 
 import java.util.Collection;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -38,6 +45,7 @@ public class ExtendedListImpl implements List {
     protected static final String RESOURCE_TYPE = "aempractice/components/list";
 
     private static final String PV_CUSTOM_LINK = "customstatic";
+    public static final String PN_LINK_URL = "linkURL";
 
     @ValueMapValue
     private String listFrom;
@@ -52,6 +60,12 @@ public class ExtendedListImpl implements List {
 
     @Self
     private SlingHttpServletRequest request;
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    @Self
+    protected LinkManager linkManager;
 
     @Override
     public String getExportedType() {
@@ -75,24 +89,60 @@ public class ExtendedListImpl implements List {
                 .filter(Resource::hasChildren)
                 .map(r -> r.getChild(PV_CUSTOM_LINK));
 
-
         if (linkMultifieldOptional.isEmpty()) {
-            log.debug("No '{}' multifield found or it has no children.", PV_CUSTOM_LINK);
             return java.util.List.of();
         }
 
         Resource multifieldResource = linkMultifieldOptional.get();
         Collection<ListItem> items = StreamSupport.stream(multifieldResource.getChildren().spliterator(), false)
-                .map(child -> {
-                    ValueMap vm = child.getValueMap();
-                    String link = vm.get("linkURL", String.class);
-                    String text = vm.get("linkText", String.class);
-                    return new StaticListItemImpl(text, link);
-                })
+                .map(this::formStaticLink)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
 
         log.debug("Retrieved {} custom static list items.", items.size());
         return java.util.List.copyOf(items);
+    }
+
+    private StaticListItemImpl formStaticLink(Resource linkResource) {
+        ValueMap vm = linkResource.getValueMap();
+        String linkValue = vm.get(PN_LINK_URL, String.class);
+        String textValue = vm.get("linkText", String.class);
+        if(linkValue == null){
+            return null;
+        }
+
+        if (isExternalLink(linkValue)) {
+            Link link = linkManager.get(linkValue).build();
+            if(link.isValid()) {
+                return new StaticListItemImpl(textValue, linkValue);
+            }
+        } else {
+            Resource resource = resourceResolver.getResource(linkValue);
+            if (null != resource) {
+                if (resource.adaptTo(Page.class) != null) {
+                    Page page = resource.adaptTo(Page.class);
+                    Link link = linkManager.get(page).build();
+                    textValue = textValue == null ? getPageTitle(page) : textValue;
+                    linkValue = link.getURL();
+                    return new StaticListItemImpl(textValue, linkValue);
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getPageTitle(Page page) {
+        if(page.getNavigationTitle() != null){
+            return page.getNavigationTitle();
+        } else if(page.getTitle() != null){
+            return page.getTitle();
+        } else {
+            return page.getName();
+        }
+    }
+
+    public static boolean isExternalLink(String url) {
+        return StringUtils.isNotEmpty(url) && !url.startsWith("/");
     }
 
     private interface DelegationExclusion {
